9d4f3519ea54023e7beb7f841a6bd78330cff6b5
jhaviplav1996@gmail.com <viplavjha1993@gmail.com>
2023-09-02 13:00:46 +0530

    commit day1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CourseList from "../CourseList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/enquiries">Enquiries</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact>
+          <CourseList courses={coursesData} />
+        </Route>
+      </div>
+    </Router>
   );
 }
+
+const coursesData = [
+  {
+    id: 1,
+    title: "Introduction to React",
+    description: "Learn the basics of React.",
+  },
+  {
+    id: 2,
+    title: "Intermediate React",
+    description: "Dive deeper into React concepts.",
+  },
+];
 
 export default App;
